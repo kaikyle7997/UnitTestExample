a8a3f9761fe947db90d2157fdfe7954a2bc49fb6
kaikyle7997 <kasimjm7997@gmail.com>
2014-11-14 22:15:22 +0800

    Add WCF Example

---
+++ WcfServiceSln/WCFServiceTest/MyServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCFService;
+
+namespace WCFServiceTest
+{
+    [TestClass]
+    public class MyServiceTest
+    {
+        private MyService target;
+
+        public MyServiceTest()
+        {
+            target = new MyService();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void GetDataTest1()
+        {
+            string expected = "You entered: 92525";
+
+            string actual = target.GetData(92525);
+
+            Assert.AreEqual(expected, actual);           
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void GetDataTest2()
+        {
+            string expected = "owo";
+
+            string actual = target.GetData(9999);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void GetDataUsingDataContractTest()
+        {
+            string expected = "Hi Cubee!Suffix";
+
+            CompositeType ctInput = new CompositeType();
+            ctInput.StringValue = "Hi Cubee!";
+            ctInput.BoolValue = true;
+
+            CompositeType actual = target.GetDataUsingDataContract(ctInput);
+
+            Assert.AreEqual(expected, actual.StringValue);
+        }
+    }
+}
